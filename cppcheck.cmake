69dd261e32da997a7f4f5f8b4e8e09c8ba32e6f9
DavidVaknin <37977544+DavidVaknin@users.noreply.github.com>
2018-04-10 15:29:42 +0300

    Update cppcheck.cmake

--- cppcheck.cmake
+++ cppcheck.cmake
@@ -4,7 +4,7 @@
 # HACK this workaround is required to avoid qml files checking ^_^
 file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
 foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
-    string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
+    string(FIND ${SOURCE_FILE} "${PROJECT_TRDPARTY_DIR}" PROJECT_TRDPARTY_DIR_FOUND)
     if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
         list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
     endif ()
